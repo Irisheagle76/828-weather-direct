a165a37492d32e79df9d645b263074bd6a4e8ffc
Tim B. <tballisty@gmail.com>
2026-03-17 17:28:15 -0400

    Update substack-api.js

--- public/js/substack-api.js
+++ public/js/substack-api.js
@@ -1,3 +1,5 @@
+// substack-api.js
+// Fetches ONLY the latest Substack Article (Notes removed)
 
 export async function fetchSubstackLatestArticle() {
   const res = await fetch("/api/substack-articles");
