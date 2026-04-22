733de8dd25a8eed197c525e77d067acbc4b5580a
Irisheagle <your-email@example.com>
2026-04-22 18:08:36 -0400

    we'll see what happens

--- public/js/modules/renderSubstackV2.js
+++ public/js/modules/renderSubstackV2.js
@@ -53,10 +53,10 @@ container.innerHTML = `
       <span class="update-title-text">828 Weather Update</span>
 
       <img 
-        src="https://res.cloudinary.com/dz45rrije/image/upload/w_36,h_36,c_fit/v1776884962/substack_avlweather.png" 
-        class="substack-icon" 
-        alt="Substack"
-      />
+  src="https://res.cloudinary.com/dz45rrije/image/upload/v1776884962/substack_avlweather.png"
+  class="substack-icon"
+  alt="Substack"
+/>
 
       ${isNew ? `<span class="update-new">NEW</span>` : ""}
     </div>
