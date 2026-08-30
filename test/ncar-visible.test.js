6fb4b4fd7f3d72b66e194b1e6e75a31197c572c7
Tim Ballisty <tballisty@gmail.com>
2026-08-30 08:33:34 -0400

    Support NCAR satellite frame arrays

--- test/ncar-visible.test.js
+++ test/ncar-visible.test.js
@@ -16,8 +16,27 @@ test("NCAR visible parser returns the current Charlotte-region color image", () 
   );
 });
 
+test("NCAR visible parser supports the SciAnimator images array", () => {
+  const html = `
+    <script>
+      $("#animation").scianimator({
+        "images": [
+          "../data/satellite/20260830/CLT/color/20260830_120236_CLT.jpg",
+          "../data/satellite/20260830/CLT/color/20260830_120736_CLT.jpg"
+        ]
+      });
+    </script>
+  `;
+
+  assert.equal(
+    parseVisibleImage(html),
+    "https://weather.rap.ucar.edu/data/satellite/20260830/CLT/color/20260830_120736_CLT.jpg"
+  );
+});
+
 test("NCAR visible parser rejects images outside the expected feed", () => {
   assert.equal(parseVisibleImage('<img src="https://example.com/20260818_160236_CLT.jpg">'), null);
+  assert.equal(parseVisibleImage('"https://example.com/data/satellite/20260818/CLT/color/20260818_160236_CLT.jpg"'), null);
   assert.equal(parseVisibleImage("<html></html>"), null);
 });
 
